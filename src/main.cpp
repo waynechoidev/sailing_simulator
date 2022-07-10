39998afebb18f1f703e4c6a947fd0a7b89a1aa5f
radicalDilettante <wonjun92@gmail.com>
2022-07-10 14:01:26 +1200

    finish setup boilerplate

--- src/main.cpp
+++ src/main.cpp
@@ -58,7 +58,7 @@ int main(void)
 	glBindVertexArray(VertexArrayID);
 
 	// Create and compile our GLSL program from the shaders
-	GLuint programID = LoadShaders("vertex.shader", "fragment.shader");
+	GLuint programID = LoadShaders("vertex.glsl", "fragment.glsl");
 
 
 	static const GLfloat g_vertex_buffer_data[] = {
