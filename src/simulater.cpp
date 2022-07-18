ea33aa55e356f0741254c71b79dad130e17d9e68
radicalDilettante <wonjun92@gmail.com>
2022-07-18 15:24:55 +1200

    refactor

--- src/simulater.cpp
+++ src/simulater.cpp
@@ -9,51 +9,6 @@
 
 void Simulater::update()
 {
-
-    GLuint VertexArrayID;
-    glGenVertexArrays(1, &VertexArrayID);
-    glBindVertexArray(VertexArrayID);
-
-    // Create and compile our GLSL program from the shaders
-    GLuint programID = LoadShaders("shader/vertex.glsl", "shader/fragment.glsl");
-
-    static const GLfloat g_vertex_buffer_data[] = {
-        -1.0f,
-        -1.0f,
-        0.0f,
-
-        1.0f,
-        -1.0f,
-        0.0f,
-
-        0.0f,
-        1.0f,
-        0.0f,
-    };
-    static const GLfloat g_color_buffer_data[] = {
-        1.0f,
-        0.0f,
-        0.0f,
-
-        0.0f,
-        1.0f,
-        0.0f,
-
-        0.0f,
-        0.0f,
-        1.0f,
-    };
-
-    GLuint vertexbuffer;
-    glGenBuffers(1, &vertexbuffer);
-    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
-
-    GLuint colorbuffer;
-    glGenBuffers(1, &colorbuffer);
-    glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
-
     const float toRadians = 3.14159265f / 180.0f;
 
     bool direction = true;
@@ -61,6 +16,74 @@ void Simulater::update()
     float triMaxOffset = 0.7f;
     float triIncreasement = 0.001f;
     int currentAngle = 0;
+
+    GLuint VAO, vertexBuffer, colorBuffer, IBO, shader, u_model;
+
+    // Create and compile our GLSL program from the shaders
+    shader = LoadShaders("shader/vertex.glsl", "shader/fragment.glsl");
+
+    static const GLfloat vertices[] = {
+        -1.0f,
+        -1.0f,
+        0.0f,
+
+        1.0f,
+        -1.0f,
+        0.0f,
+
+        0.0f,
+        1.0f,
+        0.0f,
+    };
+    static const GLfloat colors[] = {
+        1.0f,
+        0.0f,
+        0.0f,
+
+        0.0f,
+        1.0f,
+        0.0f,
+
+        0.0f,
+        0.0f,
+        1.0f,
+    };
+
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    glGenBuffers(1, &vertexBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glGenBuffers(1, &colorBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, colorBuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(
+        0,        // attribute 0. No particular reason for 0, but must match the layout in the shader.
+        3,        // size
+        GL_FLOAT, // type
+        GL_FALSE, // normalized?
+        0,        // stride
+        (void *)0 // array buffer offset
+    );
+
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(
+        1,        // attribute. No particular reason for 1, but must match the layout in the shader.
+        3,        // size
+        GL_FLOAT, // type
+        GL_FALSE, // normalized?
+        0,        // stride
+        (void *)0 // array buffer offset
+    );
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    glBindVertexArray(0);
+
     do
     {
         if (direction)
@@ -82,7 +105,7 @@ void Simulater::update()
         glClear(GL_COLOR_BUFFER_BIT);
 
         // Use our shader
-        glUseProgram(programID);
+        glUseProgram(shader);
 
         glm::mat4 model(1.0f);
         model = glm::translate(model, glm::vec3(0.0f, triOffset, 0.0f));
@@ -90,38 +113,20 @@ void Simulater::update()
         model = glm::scale(model, glm::vec3(triOffset, triOffset, triOffset));
 
         // move forward
-        GLint u_model = glGetUniformLocation(programID, "model");
+        u_model = glGetUniformLocation(shader, "model");
         if (u_model != -1)
             glUniformMatrix4fv(u_model, 1, GL_FALSE, glm::value_ptr(model));
 
-        // 1rst attribute buffer : vertices
-        glEnableVertexAttribArray(0);
-        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-        glVertexAttribPointer(
-            0,        // attribute 0. No particular reason for 0, but must match the layout in the shader.
-            3,        // size
-            GL_FLOAT, // type
-            GL_FALSE, // normalized?
-            0,        // stride
-            (void *)0 // array buffer offset
-        );
-
-        glEnableVertexAttribArray(1);
-        glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
-        glVertexAttribPointer(
-            1,        // attribute. No particular reason for 1, but must match the layout in the shader.
-            3,        // size
-            GL_FLOAT, // type
-            GL_FALSE, // normalized?
-            0,        // stride
-            (void *)0 // array buffer offset
-        );
+        glBindVertexArray(VAO);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+        glBindBuffer(GL_ARRAY_BUFFER, colorBuffer);
 
         // Draw the triangle !
-        glDrawArrays(GL_TRIANGLES, 0, 3 * 12); // 3 indices starting at 0 -> 1 triangle
+        glDrawArrays(GL_TRIANGLES, 0, 3); // 3 indices starting at 0 -> 1 triangle
 
-        glDisableVertexAttribArray(0);
-        glDisableVertexAttribArray(1);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+        glBindVertexArray(0);
+        glUseProgram(0);
 
         // Swap buffers
         glfwSwapBuffers(_window);
@@ -132,10 +137,10 @@ void Simulater::update()
            glfwWindowShouldClose(_window) == 0);
 
     // Cleanup VBO
-    glDeleteBuffers(1, &vertexbuffer);
-    glDeleteBuffers(1, &colorbuffer);
-    glDeleteVertexArrays(1, &VertexArrayID);
-    glDeleteProgram(programID);
+    glDeleteBuffers(1, &vertexBuffer);
+    glDeleteBuffers(1, &colorBuffer);
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteProgram(shader);
 
     // Close OpenGL window and terminate GLFW
     glfwTerminate();
