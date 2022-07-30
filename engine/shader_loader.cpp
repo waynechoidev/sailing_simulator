60700685d59a8808616e06095c41efae454e46bb
radicalDilettante <wonjun92@gmail.com>
2022-07-30 21:24:22 +1200

    fix dependency

--- engine/shader_loader.cpp
+++ engine/shader_loader.cpp
@@ -1,15 +1,7 @@
-#include <stdio.h>
-#include <string>
 #include <vector>
 #include <iostream>
 #include <fstream>
-#include <algorithm>
 #include <sstream>
-using namespace std;
-
-#include <stdlib.h>
-#include <string.h>
-
 #include <GL/glew.h>
 
 #include "shader_loader.h"
