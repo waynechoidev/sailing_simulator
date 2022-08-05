9e77d150e8505425217628317f213bdfb933cb72
radicalDilettante <wonjun92@gmail.com>
2022-08-05 18:26:50 +1200

    build

---
+++ web/out/_next/static/h0ZoHAWgQUj6b3XzuYUVc/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-c45a21f58b965394.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
