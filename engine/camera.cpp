1b8a4815de11a1bd473933604a143f148c2dee0a
radicalDilettante <wonjun92@gmail.com>
2022-08-20 22:03:07 +1200

    change camera position

--- engine/camera.cpp
+++ engine/camera.cpp
@@ -15,7 +15,7 @@ glm::mat4 Camera::calculateViewMatrix()
 
 void Camera::update(glm::vec3 startTarget)
 {
-	position = {startTarget.x, startTarget.y - 50.0f, 100.0f};
+	position = {startTarget.x, startTarget.y - 100.0f, 30.0f};
 	target = startTarget;
 }
 
